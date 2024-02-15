52225baa0b8769f067a3781c43315d4d783ab36f
mehmet-mhy <mehmet.mhy@gmail.com>
2024-02-14 21:42:41 -0700

    backup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Builder, By } = require('selenium-webdriver');
+const XLSX = require('xlsx');
 
 async function fetchAndExtractData(url, timeDelay=10000) {
     let output = {}
@@ -56,12 +57,83 @@ async function fetchAndExtractData(url, timeDelay=10000) {
     return output
 }
 
-async function main(){
-    const url = 'https://www.berkeley.edu/directory/?search-term=rinaest@berkeley.edu'
-    const timeDelay = 1 // seconds
-    const output = await fetchAndExtractData(url, timeDelay*1000)
-    console.log(JSON.stringify(output, null, indent=4))
+function readXlsxFile(filePath) {
+    // read the XLSX file
+    const workbook = XLSX.readFile(filePath);
+
+    // get the name of the first sheet
+    const sheetName = workbook.SheetNames[0];
+
+    // convert the sheet to JSON
+    const sheetJson = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+    return sheetJson;
 }
+
+// {
+//     "Attention Name": " Cox Lab, RM 375E",
+//     "Row Labels": "okie@berkeley.edu",
+//     " Sum of Ext Price ": 562.79,
+//     "First Order": 44201.59869212963,
+//     "Last Order": 44321.89335648148
+// },
+// {
+//     "Attention Name": " Nicholas Hadler",
+//     X "First Name": "Nicholas ",
+//     X "Last Name": "Hadler",
+//     X "Address": "CCHEM - Dept Of Chemistry",
+//     "Row Labels": "nhadler@berkeley.edu",
+//     " Sum of Ext Price ": 558.12,
+//     "First Order": 44964.886979166666,
+//     "Last Order": 45125.880949074075
+// },
+
+async function main() {
+    const spreadSheet = readXlsxFile('ucb_customer_list.xlsx')
+    for(let row of spreadSheet) {
+        if (!Object.keys(row).includes("Attention Name")) {
+            continue
+        }
+
+        const email = row["Row Labels"].replaceAll(" ", "")
+        const url = `https://www.berkeley.edu/directory/?search-term=${email}`
+
+        const scrape = await fetchAndExtractData(url, 1*1000)
+
+        if (scrape === undefined) {
+            console.log(`error - failed to scrape ${url}`)
+            continue
+        }
+
+        // extract first and last name from scrape
+        let fullName = scrape["name"].split(" ")
+        let firstName = ""
+        let lastName = ""
+        if (fullName.length === 3) {
+            firstName = fullName[0]
+            lastName = fullName[2]
+        } else if (fullName.length === 2) {
+            firstName = fullName[0]
+            lastName = fullName[1]
+        }
+
+        row["First Name"] = firstName
+        row["Last Name"] = lastName
+        row["Address"] = scrape["homeDepartment"]
+
+        console.log(JSON.stringify(row, null, indent=4))
+        console.log("\n")
+        break
+    }
+}
+
+// async function main(){
+//     const url = 'https://www.berkeley.edu/directory/?search-term=jfletcher@berkeley.edu'
+//     const timeDelay = 1 // seconds
+//     const output = await fetchAndExtractData(url, timeDelay*1000)
+//     console.log(JSON.stringify(output, null, indent=4))
+// }
 
 // main function calls
 main().then()
+
